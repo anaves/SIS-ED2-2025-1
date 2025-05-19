8d340db7ae42a4cafef089c8c8e9f4f006d541a6
Alysson Naves <alysson.naves@ectare.com.br>
2025-05-19 20:44:46 -0300

    feat: aula 19/05/2025

--- aulas/_06_TabelaHash/src/App.java
+++ aulas/_06_TabelaHash/src/App.java
@@ -2,6 +2,8 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JOptionPane;
+
 public class App {
 
     public static int funcaoHash(String palavra, int tam){
@@ -10,14 +12,18 @@ public class App {
         // A -> valor 65
         /// 65 -> 0
         /// 90 -> 25
-        hash = 13+palavra.toUpperCase().charAt(0);  // pega a inicial
-
+        //hash = 13+palavra.toUpperCase().charAt(0);  // pega a inicial
+        palavra = palavra.toUpperCase();
+        for (int i = 0; i < palavra.length(); i++) {
+            hash += palavra.charAt(i);
+        }
         return hash % tam;
     }
 
     public static boolean buscarPalavra(String palavra, ArrayList<String>[] tabelaHash, int tam) {
-	    
-        return true;
+	    int categoria = funcaoHash(palavra, tam);
+        List<String> elementos = tabelaHash[categoria];
+        return elementos.contains(palavra.toUpperCase());
     }
     
     public static void main(String[] args) throws Exception {
@@ -34,6 +40,8 @@ public class App {
         }
         System.out.println("Adicionando palavras...");
         List<String> listaPalavra = GeradorPalavras.lerPalavras();
+
+        System.out.println("TOTAL: " + listaPalavra.size());
         for (String palavra : listaPalavra) {
             palavra = palavra.toUpperCase();
             int categoria = funcaoHash(palavra, totalCategorias);
@@ -47,8 +55,41 @@ public class App {
         System.out.println("---PREENCHIDA---");
         for (int i = 0; i < tabelaHash.length; i++) {
             ArrayList<String> el = tabelaHash[i];
-            String letra = el.get(0);
-            System.out.println(i + " "+ letra + "-> total = " + el.size());
+            //String letra = el.get(0);
+            // el.subList(0, 5)
+            System.out.println(i + " " + "-> total = " + el.size());
         }
+
+        int opcao = 0;
+        do{
+            String procurar = JOptionPane.showInputDialog("Digite: ");
+            boolean result = buscarPalavra(procurar, tabelaHash, totalCategorias);
+            if (result){
+                JOptionPane.showMessageDialog(null, "achei " + procurar);
+            }else{
+                JOptionPane.showMessageDialog(null, "nao achei " + procurar);
+            }
+            opcao = JOptionPane.showConfirmDialog(null, "Continuar");
+
+        }while(opcao == JOptionPane.OK_OPTION);
+
+
+        // teste de desempenho
+        long inicio = System.currentTimeMillis();
+        for (String palavra : listaPalavra) {
+            // teste obvio
+            listaPalavra.contains(palavra.toUpperCase());
+        }
+        long fim = System.currentTimeMillis();
+        System.out.println("Busca ArrayList "+(fim-inicio)+ " ms");
+
+
+        inicio = System.currentTimeMillis();
+        for (String palavra : listaPalavra) {
+            // teste obvio
+            buscarPalavra(palavra, tabelaHash, totalCategorias);
+        }
+        fim = System.currentTimeMillis();
+        System.out.println("Busca Tabela Hash "+(fim-inicio)+ " ms");
     }
 }
